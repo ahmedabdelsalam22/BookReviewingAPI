3f432672aa22efc2f6bc55fb90054c1b56971146
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-28 23:00:42 +0300

    install needed packages and create ApplicationDbContext

---
+++ BookReviewingAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookReviewingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookReviewingAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+    }
+
+}
