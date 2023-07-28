ff36cd9532d59bd44999bfffb658a2d913dc6c2a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-28 20:54:49 +0300

    Book Model

---
+++ BookReviewingAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookReviewingAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        public DateTime DatePublished { get; set; }
+    }
+}
