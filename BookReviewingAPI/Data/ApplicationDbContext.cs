35b3fe1f4cfdd0ebad4f0a5df60a7c926d25c6a0
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-28 23:03:44 +0300

    OnModelCreating and fluent api

--- BookReviewingAPI/Data/ApplicationDbContext.cs
+++ BookReviewingAPI/Data/ApplicationDbContext.cs
@@ -18,6 +18,33 @@ namespace BookReviewingAPI.Data
         public DbSet<Reviewer> Reviewers { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }
         public DbSet<BookCategory> BookCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BookCategory>()
+                        .HasKey(bc => new { bc.BookId, bc.CategoryId });
+            modelBuilder.Entity<BookCategory>()
+                        .HasOne(b => b.Book)
+                        .WithMany(bc => bc.BookCategories)
+                        .HasForeignKey(b => b.BookId);
+            modelBuilder.Entity<BookCategory>()
+                        .HasOne(c => c.Category)
+                        .WithMany(bc => bc.BookCategories)
+                        .HasForeignKey(c => c.CategoryId);
+
+            modelBuilder.Entity<BookAuthor>()
+                        .HasKey(ba => new { ba.BookId, ba.AuthorId });
+            modelBuilder.Entity<BookAuthor>()
+                        .HasOne(a => a.Author)
+                        .WithMany(ba => ba.BookAuthors)
+                        .HasForeignKey(a => a.AuthorId);
+            modelBuilder.Entity<BookAuthor>()
+                        .HasOne(b => b.Book)
+                        .WithMany(ba => ba.BookAuthors)
+                        .HasForeignKey(b => b.BookId);
+        }
+
     }
 
 }
