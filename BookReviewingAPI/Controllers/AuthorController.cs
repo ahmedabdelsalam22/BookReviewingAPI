7769845e623a977aa7d8a129275c7abdd99e0de7
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-29 20:40:18 +0300

    create  authorController and create first endPoint

---
+++ BookReviewingAPI/Controllers/AuthorController.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using BookReviewingAPI.Models;
+using BookReviewingAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BookReviewingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorRepository _repository;
+        private APIResponse _response; 
+        public AuthorController(IAuthorRepository repository)
+        {
+            _repository = repository;
+            _response = new APIResponse();
+        }
+        [HttpGet("allAuthors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<APIResponse> GetAllAuthors()
+        {
+            try
+            {
+                IEnumerable<Author> authors = await _repository.GetAllAsync();
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = authors;
+                return _response;
+            }
+            catch (Exception e)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { e.ToString() };
+                return _response;
+            }
+        }
+        
+    }
+}
