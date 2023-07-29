abbb2f19ea3fb86180897c2bc3d0994e389e59fe
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-30 02:13:52 +0300

    CategoryRepository

---
+++ BookReviewingAPI/Repository/IRepositoryImpl/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookReviewingAPI.Data;
+using BookReviewingAPI.Models;
+using BookReviewingAPI.Repository.IRepository;
+
+namespace BookReviewingAPI.Repository.IRepositoryImpl
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void UpdateAsync(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
