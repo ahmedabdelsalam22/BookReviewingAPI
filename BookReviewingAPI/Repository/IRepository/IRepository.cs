7a0f983bd4e6612780d5e360d8a16656d28166e9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-29 16:29:34 +0300

    create IRepository

---
+++ BookReviewingAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace BookReviewingAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>>? filter = null);
+        Task<T> GetAsync(int id);
+        Task DeleteAsync(T entity);
+        Task<T> CreateAsync(T entity);
+        Task SaveChanges();
+    }
+}
