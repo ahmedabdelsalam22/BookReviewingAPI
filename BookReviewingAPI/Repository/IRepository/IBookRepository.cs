cf09a2d4448f1e3b8b5e76b8d7c9448f413d7de9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-29 16:59:00 +0300

    create IBookRepository

---
+++ BookReviewingAPI/Repository/IRepository/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookReviewingAPI.Models;
+using BookReviewingAPI.Repository.IRepositoryImpl;
+
+namespace BookReviewingAPI.Repository.IRepository
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+        Task UpdateAsync(Book book);
+    }
+}
