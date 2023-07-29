143ca324e04cfa7c56476e754c530feff37bb86d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-29 16:46:00 +0300

    create RepositoryImpl

--- BookReviewingAPI/Repository/IRepository/IRepository.cs
+++ BookReviewingAPI/Repository/IRepository/IRepository.cs
@@ -4,10 +4,10 @@ namespace BookReviewingAPI.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>>? filter = null);
-        Task<T> GetAsync(int id);
-        Task DeleteAsync(T entity);
-        Task<T> CreateAsync(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T,bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>>? filter = null,bool tracked = true);
+        void Delete(T entity);
+        Task CreateAsync(T entity);
         Task SaveChanges();
     }
 }
