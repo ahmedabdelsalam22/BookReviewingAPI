05e7a5407abc323793ce3aa88a473de2b9abe12d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-30 17:14:11 +0300

    GetCountryByAuthorId endpoint

--- BookReviewingAPI/Controllers/CountryController.cs
+++ BookReviewingAPI/Controllers/CountryController.cs
@@ -4,6 +4,7 @@ using BookReviewingAPI.Repository.IRepository;
 using BookReviewingAPI.Repository.IRepositoryImpl;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
 using System.Net;
 
 namespace BookReviewingAPI.Controllers
@@ -12,22 +13,29 @@ namespace BookReviewingAPI.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
-        private readonly ICountryRepository _repository;
+        private readonly ICountryRepository _countryRepository;
+        private readonly IAuthorRepository _authorRepository;
         private APIResponse _response;
 
-        public CountryController(ICountryRepository repository)
+        public CountryController(ICountryRepository repository,IAuthorRepository authorRepository)
         {
-            _repository = repository;
+            _countryRepository = repository;
             _response = new APIResponse();
+            _authorRepository = authorRepository;
         }
         [HttpGet("allCountries")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<APIResponse> GetAllCountries()
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetAllCountries()
         {
             try
             {
-                IEnumerable<Country> countries = await _repository.GetAllAsync();
+                IEnumerable<Country> countries = await _countryRepository.GetAllAsync();
+                if (countries == null) 
+                {
+                    return NotFound("No countries exists with this id");
+                }
                 _response.StatusCode = HttpStatusCode.OK;
                 _response.IsSuccess = true;
                 _response.Result = countries;
@@ -44,6 +52,7 @@ namespace BookReviewingAPI.Controllers
         [HttpGet("country/{countryId}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<APIResponse>> GetCountryById(int countryId)
         {
             try
@@ -52,7 +61,7 @@ namespace BookReviewingAPI.Controllers
                 {
                     return BadRequest();
                 }
-                Country? country = await _repository.GetAsync(filter: x => x.Id == countryId, tracked: false);
+                Country? country = await _countryRepository.GetAsync(filter: x => x.Id == countryId, tracked: false);
                 if (country == null)
                 {
                     return NotFound("No countries exists with this id");
@@ -71,6 +80,40 @@ namespace BookReviewingAPI.Controllers
                 return _response;
             }
 
+        }
+        [HttpGet("{authorId}/country")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> GetCountryByAuthorId(int authorId)
+        {
+            try 
+            {
+                if (authorId == 0)
+                {
+                    return BadRequest();
+                }
+                Author? author = await _authorRepository.GetAsync(filter: x => x.Id == authorId, tracked: false, includeProperties: "Country");
+
+                Country country = author.Country;
+
+                if (author == null)
+                {
+                    return NotFound("No authors exists with this id");
+                }
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = country;
+
+                return _response;
+            }
+            catch (Exception e)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { e.ToString() };
+                return _response;
+            }
         }
     }
 }
