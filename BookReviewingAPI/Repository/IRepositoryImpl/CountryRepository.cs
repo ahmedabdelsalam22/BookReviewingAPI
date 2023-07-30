21d959570b8083a46a1cff26f565ef93cb74d08d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-30 16:56:52 +0300

    create CountryRepository

---
+++ BookReviewingAPI/Repository/IRepositoryImpl/CountryRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookReviewingAPI.Data;
+using BookReviewingAPI.Models;
+using BookReviewingAPI.Repository.IRepository;
+
+namespace BookReviewingAPI.Repository.IRepositoryImpl
+{
+    public class CountryRepository : Repository<Country>, ICountryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CountryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void UpdateAsync(Country country)
+        {
+            _db.Countries.Update(country);
+        }
+    }
+}
