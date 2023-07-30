39f226daa6b15377c81bfd05456601244df648ff
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-30 21:48:01 +0300

    fix bug in GetAuthorsByCountryId endPoint in CountryController

--- BookReviewingAPI/Controllers/CountryController.cs
+++ BookReviewingAPI/Controllers/CountryController.cs
@@ -4,12 +4,15 @@ using BookReviewingAPI.Repository.IRepository;
 using BookReviewingAPI.Repository.IRepositoryImpl;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics.Metrics;
 using System.Net;
+using System.Xml;
+using Formatting = Newtonsoft.Json.Formatting;
 
 namespace BookReviewingAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[Controller]/")]
     [ApiController]
     public class CountryController : ControllerBase
     {
@@ -85,6 +88,7 @@ namespace BookReviewingAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<APIResponse>> GetCountryByAuthorId(int authorId)
         {
             try 
@@ -93,15 +97,74 @@ namespace BookReviewingAPI.Controllers
                 {
                     return BadRequest();
                 }
-                Author? author = await _authorRepository.GetAsync(filter: x => x.Id == authorId, tracked: false, includeProperties: "Country");
-
+                Author? author = await _authorRepository.GetAsync(filter: x => x.Id == authorId, includeProperties: "Country");
                 if (author == null)
                 {
                     return NotFound("No authors exists with this id");
                 }
+                Country country = author.Country;
+                if (country == null)
+                {
+                    return NotFound("No countries exists with this id");
+                }
                 _response.StatusCode = HttpStatusCode.OK;
                 _response.IsSuccess = true;
                 _response.Result = country;
+                return _response;
+            }
+            catch (Exception e)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { e.ToString() };
+                return _response;
+            }
+        }
+
+        /*
+         * string json = JsonConvert.SerializeObject(author, Formatting.Indented, new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+         */
+
+        // Country country = JsonConvert.DeserializeObject<Country>(json);
+
+        [HttpGet("{countryId}/authors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //TODO:FIX THIS BUG
+        public async Task<ActionResult<APIResponse>> GetAuthorsByCountryId(int countryId)
+        {
+            try
+            {
+                if (countryId == 0)
+                {
+                    return BadRequest();
+                }
+                Country? country = await _countryRepository.GetAsync(filter: x => x.Id == countryId, includeProperties: "Authors");
+                if (country == null)
+                {
+                    return NotFound("No countries exists with this id");
+                }
+
+                string json = JsonConvert.SerializeObject(country, Formatting.Indented, new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+
+                List<Author>? authors = JsonConvert.DeserializeObject<List<Author>>(json);
+
+                if (authors == null)
+                {
+                    return NotFound("No authors exists with this id");
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = authors;
 
                 return _response;
             }
