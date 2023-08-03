89cbeaff300fbdd3e25b7c2e922e92503badbba6
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-03 14:22:50 +0300

    ReviewerRepository

--- BookReviewingAPI/Controllers/CountryController.cs
+++ BookReviewingAPI/Controllers/CountryController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics.Metrics;
 using System.Net;
+using System.Text.Json.Nodes;
 using System.Xml;
 using Formatting = Newtonsoft.Json.Formatting;
 
@@ -84,42 +85,48 @@ namespace BookReviewingAPI.Controllers
             }
 
         }
+      
         [HttpGet("{authorId}/country")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //TODO:FIX THIS BUG
         public async Task<ActionResult<APIResponse>> GetCountryByAuthorId(int authorId)
-        {
-            try 
-            {
-                if (authorId == 0)
-                {
-                    return BadRequest();
-                }
-                Author? author = await _authorRepository.GetAsync(filter: x => x.Id == authorId, includeProperties: "Country");
-                if (author == null)
-                {
-                    return NotFound("No authors exists with this id");
-                }
-                Country country = author.Country;
-                if (country == null)
-                {
-                    return NotFound("No countries exists with this id");
-                }
-                _response.StatusCode = HttpStatusCode.OK;
-                _response.IsSuccess = true;
-                _response.Result = country;
-                return _response;
-            }
-            catch (Exception e)
-            {
-                _response.StatusCode = HttpStatusCode.BadRequest;
-                _response.IsSuccess = false;
-                _response.ErrorMessage = new List<string> { e.ToString() };
-                return _response;
-            }
-        }
+         {
+             try 
+             {
+                 if (authorId == 0)
+                 {
+                     return BadRequest();
+                 }
+                 Author? author = await _authorRepository.GetAsync(filter: x => x.Id == authorId, includeProperties: "Country");
+                 if (author == null)
+                 {
+                     return NotFound("No authors exists with this id");
+                 }
+                 string json = JsonConvert.SerializeObject(author, Formatting.Indented, new JsonSerializerSettings
+                 {
+                     ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                 });
+                 Country? country = JsonConvert.DeserializeObject<Country>(json);
+                 if (country == null)
+                 {
+                     return NotFound("No countries exists with this id");
+                 }
+                 _response.StatusCode = HttpStatusCode.OK;
+                 _response.IsSuccess = true;
+                 _response.Result = country;
+                 return _response;
+             }
+             catch (Exception e)
+             {
+                 _response.StatusCode = HttpStatusCode.BadRequest;
+                 _response.IsSuccess = false;
+                 _response.ErrorMessage = new List<string> { e.ToString() };
+                 return _response;
+             }
+         }
 
         [HttpGet("{countryId}/authors")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -147,6 +154,7 @@ namespace BookReviewingAPI.Controllers
                 });
 
                 List<Author>? authors = JsonConvert.DeserializeObject<List<Author>>(json);
+
 
                 if (authors == null)
                 {
---
+++ BookReviewingAPI/Repository/IRepositoryImpl/ReviewerRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookReviewingAPI.Data;
+using BookReviewingAPI.Models;
+using BookReviewingAPI.Repository.IRepository;
+
+namespace BookReviewingAPI.Repository.IRepositoryImpl
+{
+    public class ReviewerRepository : Repository<Reviewer>, IReviewreRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ReviewerRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void UpdateAsync(Reviewer reviewer)
+        {
+            _db.Reviewers.Update(reviewer);
+        }
+    }
+}
