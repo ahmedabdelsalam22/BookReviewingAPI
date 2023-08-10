f148193f6d3ad590248d41e2a073edae13228915
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-10 15:16:01 +0300

    RegisterRequestDTO

---
+++ BookReviewingAPI/Models/Auth DTOS/RegisterRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BookReviewingAPI.Models.Auth_DTOS
+{
+    public class RegisterRequestDTO
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
