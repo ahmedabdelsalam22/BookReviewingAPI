d0dfb3f799df06afee0eb12e8464174c524ae5ee
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-10 13:46:18 +0300

    create IUnitOfWork and impl UnitOfWork

---
+++ BookReviewingAPI/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using BookReviewingAPI.Repository.IRepository;
+
+namespace BookReviewingAPI.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IAuthorRepository authorRepository { get; }
+        IBookRepository bookRepository { get; }
+        ICategoryRepository categoryRepository { get; }
+        ICountryRepository countryRepository { get; }
+        IReviewerRepository reviewerRepository { get; }
+        IReviewRepository reviewRepository { get; }
+        Task SaveChangesAsync();
+    }
+}
