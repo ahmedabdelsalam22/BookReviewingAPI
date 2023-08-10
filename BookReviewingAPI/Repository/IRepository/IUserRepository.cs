f5e6a28e1f35fbec419b199049c4ba0a1b7d43f6
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-10 15:19:42 +0300

    IUserRepository

---
+++ BookReviewingAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookReviewingAPI.Models;
+using BookReviewingAPI.Models.Auth_DTOS;
+
+namespace BookReviewingAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterRequestDTO registerRequestDTO);
+    }
+}
