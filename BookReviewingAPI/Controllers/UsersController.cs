83b97ce786615df3e6375685f109720e7a5c3f47
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-10 17:55:30 +0300

    Login end point in UsersController

---
+++ BookReviewingAPI/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using BookReviewingAPI.Models;
+using BookReviewingAPI.Models.Auth_DTOS;
+using BookReviewingAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BookReviewingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        protected APIResponse _response;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _response = new APIResponse();
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> Login([FromBody] LoginRequestDTO loginRequestDTO) 
+        {
+            var loginResponse = await _userRepository.Login(loginRequestDTO);
+            if (loginResponse == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { "Username or password is incorrect" };
+                return BadRequest(_response);
+            }
+
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = loginResponse;
+            return Ok(_response);
+
+        }
+
+    }
+}
