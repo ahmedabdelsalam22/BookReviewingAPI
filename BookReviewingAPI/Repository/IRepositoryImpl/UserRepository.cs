d6770ac2c8c43650b2be7e93739251ad5ab780fd
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-12 03:12:42 +0300

    fix issue in roleManager

--- BookReviewingAPI/Repository/IRepositoryImpl/UserRepository.cs
+++ BookReviewingAPI/Repository/IRepositoryImpl/UserRepository.cs
@@ -18,13 +18,16 @@ namespace BookReviewingAPI.Repository.IRepositoryImpl
         private IMapper _mapper;
         private string? _secretKey;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserRepository(ApplicationDbContext db, IMapper mapper,IConfiguration configuration, UserManager<ApplicationUser> userManager)
+        public UserRepository(ApplicationDbContext db, IMapper mapper,IConfiguration configuration,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
             _mapper = mapper;
             _secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public bool IsUniqueUser(string username)
@@ -95,6 +98,11 @@ namespace BookReviewingAPI.Repository.IRepositoryImpl
                 var result = await _userManager.CreateAsync(user, registerRequestDTO.Password);
                 if (result.Succeeded)
                 {
+                    if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult()) 
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                        await _roleManager.CreateAsync(new IdentityRole("customer"));
+                    }
                     await _userManager.AddToRoleAsync(user, "admin");
 
                     var userToReturn = _db.ApplicationUsers.FirstOrDefault(u => u.UserName == registerRequestDTO.UserName);
